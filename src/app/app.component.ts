e92e24e88d03d9132c60979f895482c94e04156f
Emad Gelani <egelani@jahez.net>
2024-08-22 12:28:06 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Hls from 'hls.js';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PlayGround.Front.HLS';
-}
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    this.loadHls();
+  }
+
+  private loadHls(): void {
+    const video = this.videoPlayer.nativeElement;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource('https://localhost:7186/Videos/index.m3u8'); // Replace with your HLS stream URL
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play();
+      });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = 'https://localhost:7186/index.m3u8'; // Replace with your HLS stream URL
+      video.addEventListener('loadedmetadata', () => {
+        video.play();
+      });
+    } else {
+      console.error('HLS is not supported in this browser.');
+    }
+  }}
